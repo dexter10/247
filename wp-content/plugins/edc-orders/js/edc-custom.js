6b0f0cf55617f0224580fc5241d5a96110a924fa
Jason Thane Jeffers <info@ordnung.nl>
2017-09-06 17:10:59 +0200

    Working - up to <carrier_service> field

---
+++ wp-content/plugins/edc-orders/js/edc-custom.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function($) {
+
+	"use strict";
+			
+	$("#ship-to-different-address-checkbox").on("change", function() {
+		if (this.checked) {
+			billingToShipping();
+		}
+		console.log('clicked');
+	});
+	
+	$("#place_order").on("click", function() {
+		if( !$('.shipping_address').is(':visible') ) {
+				billingToShipping();
+				console.log('place order');
+		}
+	});
+
+	function billingToShipping() {
+		$("[name='shipping_first_name']").val($("[name='billing_first_name']").val());
+		$("[name='shipping_last_name']").val($("[name='billing_last_name']").val());
+		$("[name='shipping_address_1']").val($("[name='billing_address_1']").val());
+		$("[name='shipping_address_house']").val($("[name='billing_address_house']").val());
+		$("[name='shipping_city']").val($("[name='billing_city']").val());
+		$("[name='shipping_postcode']").val($("[name='billing_postcode']").val());
+	}
+
+});
